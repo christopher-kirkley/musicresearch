e58abf0b5570178f7a5765e5b2eb0d38f33f34f8
Christopher Kirkley <ckirkley@gmail.com>
2020-03-30 22:39:03 -0700

    Added db connection.

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from config import Config
 
 app = Flask(__name__)
 
+config = Config()
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{config.USERNAME}:{config.PASSWORD}@localhost:5432/{config.DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+db_session = scoped_session(sessionmaker(bind=engine))
+Base = declarative_base()
+
 @app.route('/')
 def index():
-    return "hello"
+    x = db_session.execute("SELECT * FROM contact;").fetchall()
+    return str(x) 
 
 if __name__ == '__main__':
     app.run(debug=True)
